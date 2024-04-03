8110ccbb3772263a7901049b34a56e74b06a9c7a
Rajeshwari Boddupall <Rajeshwari Boddupall@LAPTOP-GTQUEF7U.Dlink>
2024-04-02 19:05:28 -0600

    deepthi

---
+++ deepthi/src/deepthi/Try.java
@@ -0,0 +1,11 @@
+package deepthi;
+
+public class Try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("lets make a change");
+
+	}
+
+}
---
+++ deepthi/src/deepthi/one.java
@@ -0,0 +1,11 @@
+package deepthi;
+
+public class one {
+
+	public static void main(String[] args) {
+		System.out.println("hi ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
