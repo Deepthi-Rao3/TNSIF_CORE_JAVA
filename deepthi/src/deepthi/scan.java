6dba91be4d4c9b228bf1d63c8195cb5dd675a7cc
Rajeshwari Boddupall <Rajeshwari Boddupall@LAPTOP-GTQUEF7U.Dlink>
2024-04-02 19:40:59 -0600

    prg1

---
+++ deepthi/src/deepthi/scan.java
@@ -0,0 +1,13 @@
+package deepthi;
+import java.util.*;
+public class scan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("enter a number");
+     Scanner sc =new Scanner(System.in);
+     int num= sc.nextInt();
+     System.out.println(num);
+	}
+
+}
